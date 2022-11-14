50de8204a9237a0da5485b0807e8ee2ab0cab565
Vladislav <tolkachew.wlad@yandex.ru>
2022-11-14 17:17:55 +0300

    sss

--- src/Pattern17.java
+++ src/Pattern17.java
@@ -1,2 +1,28 @@
-public class Patter17 {
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= rows; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = i; k <= rows; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Pattern22.java
+++ src/Pattern22.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Pattern22 {
+import java.util.Scanner;
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("0 ");
+            }
+            System.out.print(i + " ");
+            for (int k = i; k < rows; k++) {
+                System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
 }
